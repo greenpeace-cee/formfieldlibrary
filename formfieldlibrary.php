da83b0d130fe2d7d8749d45b6167635e317ba1ab
Simon Hermann <simon.hermann@civiservice.de>
2023-12-15 02:32:29 -0800

    Run civix upgrade

--- formfieldlibrary.php
+++ formfieldlibrary.php
@@ -20,7 +20,6 @@ function formfieldlibrary_civicrm_container(ContainerBuilder $container) {
   $container->setDefinition('formfieldlibrary', $definition);
 }
 
-
 /**
  * Implements hook_civicrm_config().
  *
@@ -28,15 +27,6 @@ function formfieldlibrary_civicrm_container(ContainerBuilder $container) {
  */
 function formfieldlibrary_civicrm_config(&$config) {
   _formfieldlibrary_civix_civicrm_config($config);
-}
-
-/**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function formfieldlibrary_civicrm_xmlMenu(&$files) {
-  _formfieldlibrary_civix_civicrm_xmlMenu($files);
 }
 
 /**
@@ -49,107 +39,12 @@ function formfieldlibrary_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function formfieldlibrary_civicrm_postInstall() {
-  _formfieldlibrary_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function formfieldlibrary_civicrm_uninstall() {
-  _formfieldlibrary_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function formfieldlibrary_civicrm_enable() {
   _formfieldlibrary_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function formfieldlibrary_civicrm_disable() {
-  _formfieldlibrary_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function formfieldlibrary_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _formfieldlibrary_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function formfieldlibrary_civicrm_managed(&$entities) {
-  _formfieldlibrary_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function formfieldlibrary_civicrm_caseTypes(&$caseTypes) {
-  _formfieldlibrary_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function formfieldlibrary_civicrm_angularModules(&$angularModules) {
-  _formfieldlibrary_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function formfieldlibrary_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _formfieldlibrary_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function formfieldlibrary_civicrm_entityTypes(&$entityTypes) {
-  _formfieldlibrary_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
@@ -159,9 +54,8 @@ function formfieldlibrary_civicrm_entityTypes(&$entityTypes) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function formfieldlibrary_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
